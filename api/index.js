aba56fc6d4e0c5d8baf88179b39b10c65bee74fa
IanD1013 <ian.tung1013@gmail.com>
2024-07-17 10:15:38 +1200

    Add transaction model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const Transaction = require("./models/transaction");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
@@ -10,7 +13,10 @@ app.get("/api/test", (req, res) => {
 });
 
 app.post("/api/transaction", (req, res) => {
+  console.log(process.env.MONGO_URL);
+  //const { name, description, datetime } = req.body;
   res.json(req.body);
 });
 
 app.listen(4040);
+//
