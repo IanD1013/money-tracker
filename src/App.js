35ae6fa5eb7e3f1ce3e7f5052ca9930ecc96c767
IanD1013 <ian.tung1013@gmail.com>
2024-07-16 19:11:07 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>
+        $400<span>.00</span>
+      </h1>
+
+      <form>
+        <div className="basic">
+          <input type="text" placeholder={"+200 new samsung tv"}></input>
+          <input type="datetime-local"></input>
+        </div>
+
+        <div className="description">
+          <input type="text" placeholder="description"></input>
+        </div>
+
+        <button type="submit">Add new transcation</button>
+      </form>
+
+      <div className="transcations">
+        <div className="transcation">
+          <div className="left">
+            <div className="name"> New Samsung TV</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transcation">
+          <div className="left">
+            <div className="name"> New Samsung TV</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transcation">
+          <div className="left">
+            <div className="name"> New Samsung TV</div>
+            <div className="description">It was time for new TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
